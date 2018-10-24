880207557e7b94a062f66e8657016aec413259a7
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2018-10-24 18:20:43 +1100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,25 +1,10 @@
-sudo ufw default deny incoming
-sudo ufw default deny outgoing
-sudo ufw allow in on enp5s0 from any to any port 53,67 proto udp
-sudo ufw allow in on enp5s0 from any to any port 4747 proto tcp
-sudo ufw allow out on enp5s0 from any to any port 1194 proto udp
-sudo ufw allow out on enp5s0
-sudo ufw allow out on tun0
-sudo ufw allow in on tun0 from any to any port 8999 proto tcp
-sudo ufw allow out on tun0from any to any port 8999 proto udp
-sudo ufw allow in on enp5s0 from any to any port 31400 proto tcp
-sudo ufw allow in on enp5s0 from any to any port 31400 proto udp
-sudo ufw allow in on enp5s0 from any to any port 80 proto tcp
-sudo ufw allow in on enp5s0 from any to any port 43 proto tcp
-sudo ufw allow in on enp5s0 from any to any port 443 proto tcp            
-sudo ufw allow in on enp5s0 from any to any port 51046 proto tcp
-sudo ufw allow in on enp5s0 from any to any port 8008 proto tcp
-sudo ufw allow in on enp5s0 from any to any port 5000 proto tcp
-sudo ufw allow in on enp5s0 from any to any port 52376 proto tcp
-sudo ufw allow in on tun0 from any to any port 52376 proto tcp
-sudo ufw allow in on tun0 from any to any port 51205 proto tcp
-sudo ufw allow in on tun0 from any to any port 51205 proto udp
-sudo ufw allow in on tun0 from any to any port 51046:52546 proto tcp
-sudo ufw allow in on tun0 from any to any port 51046:52546 proto udp
-sudo ufw allow out on tun0 from any to any port 51046:52546 proto tcp
-sudo ufw allow out on tun0 from any to any port 51046:52546 proto udp
+
+ufw default deny outgoing
+ufw default deny incoming
+sudo ufw allow out to 10.0.0.0/24 
+sudo ufw allow in to 10.0.0.0/24
+sudo ufw allow out to 127.0.0.0/24 
+sudo ufw allow in to 127.0.0.0/24  
+sudo ufw allow out to 46.246.123.27 port 1198 proto udp
+sudo ufw allow out on tun0 
+sudo ufw allow in on tun0 
